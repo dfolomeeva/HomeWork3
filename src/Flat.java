d6e03a21446cbc166240693f26acb7079123619e
dfolomeeva <d.folomeeva1987@gmail.com>
2024-03-17 23:50:11 +0300

    Changed name of the biggest room.

--- src/Flat.java
+++ src/Flat.java
@@ -14,8 +14,8 @@ public class Flat {
     public String getFlatDescription() {
         return String.format("Flat dimensions are %d * %d * %d. " +
                 "It has %d rooms with dimensions: two kid's rooms %d * %d * %d, " +
-                "master's bedroom %d * %d * %d, two bathrooms %d * %d * %d, living room " +
-                "with kitchen %d * %d * %d.", width1, width2, height, rooms.length,
+                "master's bedroom %d * %d * %d, two bathrooms %d * %d * %d, kitchen-diner" +
+                " %d * %d * %d.", width1, width2, height, rooms.length,
                 rooms[0].width1, rooms[0].width2, rooms[0].height, rooms[2].width1,
                 rooms[2].width2, rooms[2].height, rooms[3].width1, rooms[3].width2,
                 rooms[3].height, rooms[5].width1, rooms[5].width2, rooms[5].height);
